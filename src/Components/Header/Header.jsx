2f32a07d599755ea090c1df33b42a75adc957a4c
MengZhaoFly <1424254461@qq.com>
2020-09-24 01:01:39 +0800

    update-logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,7 +12,7 @@ const navigation = [
     content: '归档',
     path: '/'
   }
-]
+];
 
 function Header() {
   const colProps = [
@@ -22,7 +22,7 @@ function Header() {
       lg: 6,
       md: 6,
       sm: 24,
-      xs: 24,   // 小屏幕 logo 全占满
+      xs: 24, // 小屏幕 logo 全占满
     },
     {
       xxl: 20,
@@ -30,31 +30,29 @@ function Header() {
       lg: 18,
       md: 18,
       sm: 0,
-      xs: 0,  // 小屏幕 导航消失
-    },
+      xs: 0, // 小屏幕 导航消失
+    }
   ];
   const navigationNode = (
     <Menu mode="horizontal">
       {
-        navigation.map((nav, i) => {
-          return (
-            <Menu.Item key={i}>
-              <Link to={nav.path}>{ nav.content }</Link>
-            </Menu.Item>
-          )
-        })
+        navigation.map((nav, i) => (
+          <Menu.Item key={i}>
+            <Link to={nav.path}>{ nav.content }</Link>
+          </Menu.Item>
+        ))
       }
       <Menu.Item>
         <a href="https://github.com/MengZhaoFly/sword-byteDance-fe" target="_blank">github</a>
       </Menu.Item>
     </Menu>
-  )
+  );
   return (
     <header id="header" className={styles.header}>
       <Row>
         <Col {...colProps[0]}>
           <div className={styles.logo}>
-            LOGO
+            剑指前端offer
           </div>
         </Col>
         <Col {...colProps[1]}>
@@ -62,6 +60,6 @@ function Header() {
         </Col>
       </Row>
     </header>
-  )
+  );
 }
 export default (Header);
